2874be7dae2190776a09472f64bbfc3b2da01359
iago bruno <iago.silva6969@gmail.com>
2025-11-14 18:44:27 -0300

    finalização das categorias

--- src/controller/CategoryController.js
+++ src/controller/CategoryController.js
@@ -1,8 +1,26 @@
 import category from '../model/Category.js';
 
 class CategoryController {
-  index() {
-    console.log('estamos aqui category index');
+  async index(req, res) {
+    try {
+      const response = await category.read();
+
+      if (response.length === 0) {
+        return res.status(404).json({
+          message: 'Nenhum produto encontrado.',
+        });
+      }
+
+      return res.status(200).json({
+        message: 'Categorias encontradas com sucesso!',
+        data: response,
+      });
+    } catch (error) {
+      res.status(500).json({
+        message: error.message,
+        details: error.details,
+      });
+    }
   }
 
   async create(req, res) {
@@ -27,12 +45,52 @@ class CategoryController {
     }
   }
 
-  update() {
-    console.log('estamos aqui category update');
+  async update(req, res) {
+    try {
+      const { id } = req.params;
+      const response = await category.update(id, req.body);
+      return res.status(200).json({
+        message: 'Categoria editada com sucesso!.',
+        data: response,
+      });
+    } catch (error) {
+      if (error.type === 'validation') {
+        return res.status(400).json({
+          message: error.message,
+          details: error.details,
+        });
+      }
+
+      if (error.type === 'not_found') {
+        return res.status(404).json({
+          message: error.message,
+        });
+      }
+
+      return res.status(500).json({
+        message: error.message || 'Erro interno no servidor.',
+        details: error.details,
+      });
+    }
   }
 
-  delete() {
-    console.log('estamos aqui category delete');
+  async delete(req, res) {
+    const { id } = req.params;
+    try {
+      const response = await category.delete(id);
+      return res.status(200).json({
+        message: 'Categoria deletada com sucesso!.',
+        data: response,
+      });
+    } catch (error) {
+      if (error.type === 'not_found') {
+        return res.status(404).json({ message: error.message });
+      }
+      return res.status(500).json({
+        message: error.message || 'Erro interno no servidor.',
+        details: error.details,
+      });
+    }
   }
 }
 
--- src/model/Category.js
+++ src/model/Category.js
@@ -4,8 +4,20 @@ import { categorySchema } from '../schemas/category-schema.js';
 import { ZodError } from 'zod';
 
 class Category {
-  read() {
-    console.log('estamos aqui category read');
+  async read() {
+    try {
+      const categories = await prisma.categories.findMany({
+        orderBy: { id: 'desc' },
+      });
+
+      return categories;
+    } catch (error) {
+      throw {
+        type: 'internal',
+        message: 'Erro ao buscar categorias',
+        details: error.message || error,
+      };
+    }
   }
 
   async create(data) {
@@ -34,12 +46,79 @@ class Category {
     }
   }
 
-  update() {
-    console.log('estamos aqui category update');
+  async update(id, data) {
+    const formatted = formatCategoryData(data);
+    const validated = categorySchema.parse(formatted);
+    try {
+      const existing = await prisma.categories.findUnique({
+        where: { id: Number(id) },
+      });
+
+      if (!existing) {
+        throw {
+          type: 'not_found',
+          message: 'Categoria não encontrada para atualização.',
+        };
+      }
+
+      const updateCategory = await prisma.categories.update({
+        where: { id: Number(id) },
+        data: validated,
+      });
+
+      return {
+        id: updateCategory.id,
+        name: updateCategory.name,
+      };
+    } catch (error) {
+      if (error instanceof ZodError) {
+        throw {
+          type: 'validation',
+          message: 'Erro de validação nos dados da Categoria.',
+          details: error.errors,
+        };
+      }
+
+      throw {
+        type: 'internal',
+        message: 'Erro interno ao atualizar Categoria.',
+        details: error.message || error,
+      };
+    }
   }
 
-  delete() {
-    console.log('estamos aqui category delete');
+  async delete(id) {
+    try {
+      const existing = await prisma.categories.findUnique({
+        where: { id: Number(id) },
+      });
+
+      if (!existing) {
+        throw {
+          type: 'not_found',
+          message: 'Produto não encontrado.',
+        };
+      }
+
+      const categoryDeleted = prisma.categories.delete({
+        where: { id: Number(id) },
+      });
+
+      return {
+        id: categoryDeleted.id,
+        name: categoryDeleted.name,
+      };
+    } catch (error) {
+      if (error.type === 'not_found') {
+        throw error;
+      }
+
+      throw {
+        type: 'internal',
+        message: 'Erro interno ao deletar produto.',
+        details: error.message || error,
+      };
+    }
   }
 }
 
