343ba84d04340791536e73a517bdd0556a4d219e
iago bruno <iago.silva6969@gmail.com>
2026-01-07 16:27:29 -0300

    retorno do token corrigido

--- src/controllers/TokenController.js
+++ src/controllers/TokenController.js
@@ -5,8 +5,20 @@ class TokenController {
     try {
       const { email, password } = req.body;
       const response = await tokenService.login(email, password);
+
+      // const isProd = process.env.NODE_ENV === 'production';
+
+      // res.cookie('token', response, {
+      //   httpOnly: true,
+      //   secure: isProd,
+      //   sameSite: isProd ? 'none' : 'lax',
+      //   maxAge: 60 * 60 * 1000,
+      //   path: '/',
+      //   domain: isProd ? 'meu.dominio.com' : 'localhost',
+      // });
+
       res.status(200).json({
-        data: response,
+        token: response,
       });
     } catch (error) {
       next(error);
